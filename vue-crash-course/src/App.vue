394c669663e2fbd2900dc33a0258592e5e547765
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-01 21:20:08 +0100

    updating vue crash

--- vue-crash-course/src/App.vue
+++ vue-crash-course/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header title="Task Tracker"/>
-    <Tasks @delete-task="deleteTask" :tasks="tasks" />
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
 
--- vue-crash-course/src/components/Tasks.vue
+++ vue-crash-course/src/components/Tasks.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-for="task in tasks" :key="task.id">
-        <Task @delete-task="$emit('delete-task', task.id)" :task="task" />
+        <Task @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task" />
     </div>
 </template>
 
@@ -15,6 +15,6 @@ export default {
     components: {
         Task
     },
-    emits: ['delete-task'],
+    emits: ['delete-task', 'toggle-reminder'],
 }
 </script>
