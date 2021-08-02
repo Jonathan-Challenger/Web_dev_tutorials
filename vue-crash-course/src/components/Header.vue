c92081f4f45207d511d7f65813311bfe1fb7ac6b
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-02 17:55:29 +0100

    update button for reusability

--- vue-crash-course/src/components/Header.vue
+++ vue-crash-course/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
-        <Button @toggle-add-task="$emit('toggle-add-task')" 
+        <Button @btn-click="$emit('toggle-add-task')" 
         :text="showAddTask ? 'Close' : 'Add Task'" 
         :color="showAddTask ? 'red' : 'green'" 
         />
