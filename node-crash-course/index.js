2140732977a626074f1cb12b46a1b6c631b89ec5
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-08 14:42:16 +0100

    move logger content

--- node-crash-course/index.js
+++ node-crash-course/index.js
@@ -1,9 +1,2 @@
-const Logger = require('./logger');
 
-const logger = new Logger();
 
-logger.on('message', (data) => console.log('Called Listener', data));
-
-logger.log('Hello World');
-logger.log('Hello');
-logger.log('Hi');
--- node-crash-course/logger.js
+++ node-crash-course/logger.js
@@ -8,4 +8,12 @@ class Logger extends EventEmitter {
     }
 }
 
-module.exports = Logger;
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener', data));
+
+logger.log('Hello World');
+logger.log('Hello');
+logger.log('Hi');
+
+// module.exports = Logger;
