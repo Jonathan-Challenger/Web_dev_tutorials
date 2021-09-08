5fc7e0644ae49e6d20949026beea7943de3322d9
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-08 14:38:39 +0100

    finish event module using logger class as example

--- node-crash-course/index.js
+++ node-crash-course/index.js
@@ -1,6 +1,9 @@
-// Import person object
-const Person = require('./person');
+const Logger = require('./logger');
 
-const person1 = new Person('John Doe', 30);
+const logger = new Logger();
 
-person1.greeting();
+logger.on('message', (data) => console.log('Called Listener', data));
+
+logger.log('Hello World');
+logger.log('Hello');
+logger.log('Hi');
