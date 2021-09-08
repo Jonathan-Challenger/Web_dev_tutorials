a6f0e47c02fcdab4ba79258b80c4c30c025c80ff
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-08 14:50:44 +0100

    create base code for server

--- node-crash-course/index.js
+++ node-crash-course/index.js
@@ -1,2 +1,15 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.end('<h1>Homepage</h1>');
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
