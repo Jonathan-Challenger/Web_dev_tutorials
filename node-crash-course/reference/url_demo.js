2c7b975c2d32250c8451d26756852d051835f634
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-08 14:24:10 +0100

    finish url module

---
+++ node-crash-course/reference/url_demo.js
@@ -0,0 +1,32 @@
+const url = require('url');
+
+const myUrl = new URL('http://mywebsite.com:8000/hello.html?id=100&status=active');
+
+// Serialised URL
+console.log(myUrl.href);
+console.log(myUrl.toString());
+
+// Host (root domain)
+console.log(myUrl.host);
+
+// Hostname (does not get port)
+console.log(myUrl.hostname);
+
+// Pathname
+console.log(myUrl.pathname);
+
+// Serialised query
+console.log(myUrl.search);
+
+// Params object
+console.log(myUrl.searchParams);
+
+// Add params
+myUrl.searchParams.append('abc', '123');
+console.log(myUrl.searchParams);
+
+// Loop through params
+myUrl.searchParams.forEach((value, name) => {
+    console.log(`${name}: ${value}`)
+});
+
