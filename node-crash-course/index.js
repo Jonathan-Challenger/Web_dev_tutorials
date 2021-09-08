493cc4265306a1def1b5be658c3afe4177177499
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-08 15:18:43 +0100

    create server not using if statements

--- node-crash-course/index.js
+++ node-crash-course/index.js
@@ -3,6 +3,9 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+    /* 
+    USING IF STATEMENTS NOT EFFICIENT FOR MANY PAGES OR FOR USING CSS FILES
+
     if(req.url === '/') {
         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
             if(err) throw err;
@@ -28,7 +31,61 @@ const server = http.createServer((req, res) => {
         ];
         res.writeHead(200, {'Content-Type': 'application/json'});
         res.end(JSON.stringify(users));
+    } */
+
+    // Build file path
+    let filePath = path.join(
+        __dirname, 
+        'public', 
+        req.url === '/' ? 'index.html': req.url
+    );
+    
+    // Extension of file
+    let extname = path.extname(filePath);
+
+    // Initial Content type
+    let contentType = 'text/html';
+
+    // Check ext and set content type depending
+    switch(extname) {
+        case ".js":
+            contentType = "text/javascript";
+            break;
+        case ".css":
+            contentType = "text/css";
+            break;
+        case ".json":
+            contentType = "application/json";
+            break;
+        case ".png":
+            contentType = "image/png";
+            break;
+        case ".jpg":
+            contentType = "image/jpg";
+            break;
     }
+
+    // Read file
+    fs.readFile(filePath, (err, content) => {
+        if(err) {
+            if (err.code == "ENOENT") {
+                // Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'),
+                (err, content) => {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end(content, 'utf8');
+                })
+            } else {
+                // Some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            // Success
+            res.writeHead(200, {'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    });
 });
 
 const PORT = process.env.PORT || 5000;
