2705d621eed464f06532600bffdee9b36b30ad8e
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-08 15:01:32 +0100

    create 2 example html files and load them in server depending on url

--- node-crash-course/index.js
+++ node-crash-course/index.js
@@ -4,7 +4,30 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if(req.url === '/') {
-        res.end('<h1>Homepage</h1>');
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+            if(err) throw err;
+            // Add content type
+            res.writeHead(200, { 'Content-Type': 'text/html'})
+            res.end(content);
+        })
+    }
+
+    if(req.url === '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+            if(err) throw err;
+            // Add content type
+            res.writeHead(200, { 'Content-Type': 'text/html'})
+            res.end(content);
+        })
+    }
+
+    if(req.url === '/api/users') {
+        const users = [
+            { name: 'Bob Smith', age: 40},
+            { name: 'John Doe', age: 30}
+        ];
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(users));
     }
 });
 
