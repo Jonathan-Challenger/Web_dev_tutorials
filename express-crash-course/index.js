57f89af36b68cb4c5e1872f4da5c1ceb8a175047
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-09 15:03:03 +0100

    create logger middleware to log url and time

--- express-crash-course/index.js
+++ express-crash-course/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
 const members = require('./Members');
+const logger = require('./middleware/logger');
 
 const app = express();
+
+// Init middleware
+app.use(logger);
 
 /* 
 Send html file to webpage (this way is not ideal)
