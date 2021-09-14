b733b6765d41109e7e12863f09b1079525c547ab
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-14 15:41:08 +0100

    setup routing for home and users login/register

---
+++ login-tutorial/routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+
+// Login Page
+router.get('/login', (req, res) => {
+    res.render("login");
+})
+
+// Register Page
+router.get('/register', (req, res) => {
+    res.render("register");
+})
+
+module.exports = router;
