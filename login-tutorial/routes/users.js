5cde3bcd56681ada91ed3ec03530982705e81507
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-15 14:34:00 +0100

    Deal with if email already exists in db

--- login-tutorial/routes/users.js
+++ login-tutorial/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+// User model
+const User = require('../models/User');
 
 // Login Page
 router.get('/login', (req, res) => {
@@ -41,7 +43,23 @@ router.post('/register', (req, res) => {
             password2
         });
     } else {
-        res.send("pass")
+        // Validation passed
+        User.findOne({ email:email })
+            .then(user => {
+                if(user) {
+                    // User exists
+                    errors.push({msg:'Email is already registered'});
+                    res.render("register", {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    });
+                } else {
+                    
+                }
+            });
     }
 })
 
