f0b4c7b25ef3fcca9770cdc654016041f17138a8
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-15 12:17:14 +0100

    handle register validations

--- login-tutorial/routes/users.js
+++ login-tutorial/routes/users.js
@@ -5,11 +5,27 @@ const router = express.Router();
 // Login Page
 router.get('/login', (req, res) => {
     res.render("login");
-})
+});
 
 // Register Page
 router.get('/register', (req, res) => {
     res.render("register");
+});
+
+// Register Handle
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    // Check required fields
+    if (!name || !email || !password || !password2) {
+        errors.push({msg:"Please fill in all fields"});
+    }
+
+    // Check passwords match
+    if (password !== password2) {
+        errors.push({msg: "Passwords do not match"})
+    }
 })
 
 module.exports = router;
