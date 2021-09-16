be3d798ec5030a86785c098fbf5d7d3cebfb3dbb
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-16 11:06:26 +0100

    handle logout

--- login-tutorial/routes/users.js
+++ login-tutorial/routes/users.js
@@ -93,4 +93,11 @@ router.post('/login', (req, res, next) => {
     })(req, res, next);
 });
 
+// Logout Handle
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+});
+
 module.exports = router;
