70003685bbd897d546e66a78c9a2fd99759da041
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-01 15:08:53 +0100

    first php file

--- php_crash_course/index.php
+++ php_crash_course/index.php
@@ -1,5 +1,19 @@
 <?php 
 
-	echo 'hello world';
+	// echo 'Hello World!';
 
 ?>
+
+
+<!-- Embedding php into html -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title> My first PHP file </title>
+</head>
+<body>
+
+	<h1><?php echo 'Hello, this is my first PHP file'; ?></h1>
+
+</body>
+</html>
