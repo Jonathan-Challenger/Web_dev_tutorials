1a408ca7c14667a1f4f666a1d61981bd0504389c
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-04 15:04:59 +0100

    add basic styling

--- vuex-tutorial/src/App.vue
+++ vuex-tutorial/src/App.vue
@@ -14,5 +14,16 @@ export default {
 </script>
 
 <style>
+body {
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
 
+.container {
+  max-width: 100%;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
 </style>
