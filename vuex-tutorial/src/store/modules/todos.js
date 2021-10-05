163b9669ea7bb9a6d77cf0b071d04e5ae74335a2
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-05 14:07:20 +0100

    add two todo objects to state and getter

--- vuex-tutorial/src/store/modules/todos.js
+++ vuex-tutorial/src/store/modules/todos.js
@@ -1,9 +1,29 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const state = {};
+const state = {
+    todos: [
+        {
+            id: 1,
+            title: 'Todo One'
+        },
+        {
+            id: 2,
+            title: 'Todo Two'
+        }
+    ]
+};
 
-const getters = {};
+const getters = {
+    allTodos: (state) => state.todos
+};
 
 const actions = {};
 
 const mutations = {};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
