cc0c8bd6151bfd8b9b13741faf41044b34aef58a
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-06 15:18:44 +0100

    add trash icon and style

--- vuex-tutorial/src/components/Todos.vue
+++ vuex-tutorial/src/components/Todos.vue
@@ -4,6 +4,7 @@
       <div class="todos">
         <div v-for="todo in allTodos" :key="todo.id" class="todo">
           {{ todo.title }}
+          <i class="fas fa-trash-alt"/>
         </div>
       </div>
   </div>
@@ -38,6 +39,14 @@ export default {
   border-radius: 5px;
   text-align: center;
   position: relative;
+  cursor: pointer;
+}
+
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
   cursor: pointer;
 }
 </style>
