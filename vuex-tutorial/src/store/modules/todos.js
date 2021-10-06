21a8cf3f558ac528aaca21a3a7c0cd2875219ed3
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-06 15:40:39 +0100

    add filter action

--- vuex-tutorial/src/store/modules/todos.js
+++ vuex-tutorial/src/store/modules/todos.js
@@ -23,7 +23,15 @@ const actions = {
         await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
 
         commit('removeTodo', id);
-    }
+    },
+     async filterTodos({ commit }, e) {
+        // Get selected number
+        const limit = parseInt(e.target.options[e.target.options.selectedIndex].innerText);
+
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`);
+
+        commit('setTodos', response.data);
+     }
 };
 
 const mutations = {
