1afa8895325aff668d2d850ed07672e84ae5eabb
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-06 12:16:23 +0100

    add styling to todos

--- vuex-tutorial/src/components/Todos.vue
+++ vuex-tutorial/src/components/Todos.vue
@@ -18,6 +18,20 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
 
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
 </style>
