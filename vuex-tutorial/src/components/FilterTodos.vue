e1d95599374744a1211a1319d3e40e98e39aa62b
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-06 15:40:50 +0100

    create filter todos component

---
+++ vuex-tutorial/src/components/FilterTodos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      Filter Todos:
+      <select @change="filterTodos($event)">
+          <option value="200">200</option>
+          <option value="200">100</option>
+          <option value="200">50</option>
+          <option value="200">20</option>
+          <option value="200">10</option>
+          <option value="200">5</option>
+      </select>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+    name: 'FilterTodos',
+    methods: mapActions(['filterTodos'])
+}
+</script>
+
+<style>
+select {
+    margin-top: 20px;
+    padding: 6px;
+    border: 1px solid #41b883;
+}
+</style>
