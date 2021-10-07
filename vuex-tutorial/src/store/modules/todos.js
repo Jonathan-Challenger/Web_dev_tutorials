b6215f076be09b2814f96d824585063d663bea99
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-07 14:36:04 +0100

    add update action and mutation

--- vuex-tutorial/src/store/modules/todos.js
+++ vuex-tutorial/src/store/modules/todos.js
@@ -24,20 +24,32 @@ const actions = {
 
         commit('removeTodo', id);
     },
-     async filterTodos({ commit }, e) {
+    async filterTodos({ commit }, e) {
         // Get selected number
         const limit = parseInt(e.target.options[e.target.options.selectedIndex].innerText);
 
         const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`);
 
         commit('setTodos', response.data);
-     }
+    },
+    async updateTodo({ commit }, updTodo) {
+        const response = await axios.put(`https://jsonplaceholder.typicode.com/todos/${updTodo.id}`);
+
+        commit('updateTodo', response.data);
+    }
 };
 
 const mutations = {
     setTodos: (state, todos) => (state.todos = todos),
     newTodo: (state, todo) => state.todos.unshift(todo),
-    removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id)
+    removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id),
+    updateTodo: (state, updTodo) => {
+        const index = state.todos.findIndex(todo => todo.id === updTodo.id);
+
+        if (index !== -1) {
+            state.todos.splice(index, 1, updTodo);
+        }
+    }
 };
 
 export default {
