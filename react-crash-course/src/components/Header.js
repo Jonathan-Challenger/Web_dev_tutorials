b40918f301d4c81958deb1d5f6de66b002fd6b3e
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-13 14:38:19 +0100

    create onclick function and pass as prop into button

--- react-crash-course/src/components/Header.js
+++ react-crash-course/src/components/Header.js
@@ -2,12 +2,16 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
+
     return (
         <header className="header">
             {/* Can add style inline with {{}} or declare a style object and 
             bring it in with single {}*/}
             <h1>{title}</h1>
-            <Button color='green' text='Add'/>
+            <Button color='green' text='Add' onClick={onClick}/>
         </header>
     )
 }
