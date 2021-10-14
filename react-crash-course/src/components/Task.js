1675a3b904feb15fc7912e898f78232702ec527c
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-14 15:45:04 +0100

    remove duplicate classNames

--- react-crash-course/src/components/Task.js
+++ react-crash-course/src/components/Task.js
@@ -2,7 +2,7 @@ import { FaTimes } from 'react-icons/fa'
 
 const Task = ({ task, onDelete, onToggle }) => {
     return (
-        <div className="task" className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
             <h3>{task.text} <FaTimes onClick={() => onDelete(task.id)} style={{color:'red'}}/></h3>
             <p>{task.day}</p>
         </div>
