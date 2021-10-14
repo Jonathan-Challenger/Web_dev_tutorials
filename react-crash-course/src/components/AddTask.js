a6d932da252ff5fa66a68a3383d37d351e1b6cb0
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-14 15:54:36 +0100

    add onsubmit function to check if text empty then call onadd which is passed down from app, then reset inputs

--- react-crash-course/src/components/AddTask.js
+++ react-crash-course/src/components/AddTask.js
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 
-const AddTask = () => {
+const AddTask = ({ onAdd }) => {
     const [text, setText] = useState('')
     const [day, setDay] = useState('')
     const [reminder, setReminder] = useState(false)
 
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if (!text) {
+            alert("Please add a task!")
+            return
+        }
+
+        onAdd({ text, day, reminder })
+
+        setText('')
+        setDay('')
+        setReminder(false)
+    }
+
     return (
-        <form className='add-form'>
+        <form className='add-form' onSubmit={onSubmit}>
             <div className="form-control">
                 <label>Task</label>
                 <input type="text" placeholder="Add Task" value={text} onChange={(e) => setText(e.target.value)}/>
