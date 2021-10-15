a70c363231bbee7acc30fe103296d8928a3eb119
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-15 12:00:03 +0100

    bring in onAdd and showAdd and change button props depending on state

--- react-crash-course/src/components/Header.js
+++ react-crash-course/src/components/Header.js
@@ -1,17 +1,14 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log('Click')
-    }
+const Header = ({ title, onAdd, showAdd }) => {
 
     return (
         <header className="header">
             {/* Can add style inline with {{}} or declare a style object and 
             bring it in with single {}*/}
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick}/>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
         </header>
     )
 }
