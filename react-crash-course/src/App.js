9b9baf6a3ed9df85d5b1adc6925ada73707d5437
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-15 15:43:02 +0100

    create fetchtask func and use put req to toggle reminder in server

--- react-crash-course/src/App.js
+++ react-crash-course/src/App.js
@@ -24,6 +24,13 @@ function App() {
     return data
   }
 
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
   // Add task
   const addTask = async (task) => {
     const res = await fetch('http://localhost:5000/tasks', 
@@ -48,8 +55,21 @@ function App() {
   }
 
   // Toggle reminder
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder:!task.reminder} : task))
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, 
+    {method: 'PUT',
+    headers : {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder:data.reminder} : task))
   }
 
   return (
