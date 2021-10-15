4a88f54346eb2022e10a69043eb570e5102fb475
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-15 15:34:27 +0100

    add server manipulation for add and delete task functions

--- react-crash-course/src/App.js
+++ react-crash-course/src/App.js
@@ -25,14 +25,25 @@ function App() {
   }
 
   // Add task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', 
+      {method:'POST', 
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+    /* const id = Math.floor(Math.random() * 10000) + 1
     const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+    setTasks([...tasks, newTask]) */
   }
 
   // Delete task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'})
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
