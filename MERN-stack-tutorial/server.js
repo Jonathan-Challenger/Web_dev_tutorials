387fdab47965813246ff57636f8c5de096ca6434
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-18 10:40:09 +0100

    change to express json middleware

--- MERN-stack-tutorial/server.js
+++ MERN-stack-tutorial/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // Bodyparser middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // DB config
 const db = require('./config/keys').mongoURI;
