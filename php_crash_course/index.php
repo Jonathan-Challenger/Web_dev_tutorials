1fdc68918af97a1df86f2dc092a794c1fed1231a
Jonathan Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-18 11:44:50 +0100

    Add files via upload

--- php_crash_course/index.php
+++ php_crash_course/index.php
@@ -1,19 +1,61 @@
-<?php 
-
-	// echo 'Hello World!';
-
-?>
-
-
-<!-- Embedding php into html -->
 <!DOCTYPE html>
 <html>
 <head>
-	<title> My first PHP file </title>
+	<meta charset="utf-8">
+	<title>Loops</title>
 </head>
 <body>
 
-	<h1><?php echo 'Hello, this is my first PHP file'; ?></h1>
+	<div class="container">
+
+	<?php
+
+		/* For, while, do...while, foreach */
+
+		# For
+		# @params - init, condition, inc
+		
+		for ($i = 0; $i < 10; $i++) {
+			echo 'Number ' . $i;
+			echo '<br>';
+		}
+
+		# While
+		# @params - condition
+
+		$i = 0;
+
+		while($i < 10) {
+			echo $i;
+			echo '<br>';
+			$i++;
+		}
+
+		# Do...while
+		# @params - condition
+
+		$i = 0;
+
+		do {
+			echo $i;
+			echo '<br>';
+			$i++;
+		} while ($i <= 10);
+
+		# Foreach
+
+		$people = array('Brad' => 'brad@gmail.com', 'Jose' => 'jose@gmail.com', 'William' => 'william@gmail.com');
+
+		foreach ($people as $person => $email) {
+			echo $person . ': ' . $email;
+			echo '<br>';
+		}
+
+	?>
+
+	</div>
 
 </body>
 </html>
+
+
