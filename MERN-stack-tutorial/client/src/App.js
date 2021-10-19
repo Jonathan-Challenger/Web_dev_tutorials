3d18c7964a35ad4a0984110f600352ca40a4bf6e
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-19 15:27:47 +0100

    add navbar to app js

--- MERN-stack-tutorial/client/src/App.js
+++ MERN-stack-tutorial/client/src/App.js
@@ -1,9 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import AppNavbar from './components/AppNavbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <AppNavbar />
     </div>
   );
 }
