2df5ccc471f784d371bac773f824bd9bbe611c8a
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-22 12:38:42 +0100

    use useEffect to fetch tasks from api on start then create postItems and insert into return

--- redux-crash-course/src/components/Posts.js
+++ redux-crash-course/src/components/Posts.js
@@ -1,13 +1,34 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const Posts = () => {
+    const [posts, setPosts] = useState([])
+
     useEffect(() => {
-        console.log(123)
-    })
+        const getPosts = async () => {
+            const postsFromServer = await fetchPosts()
+            setPosts(postsFromServer)
+        }
+
+        getPosts()
+    }, [])
+
+    const fetchPosts = async () => {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const data = await res.json()
+        return data
+    }
+
+    const postItems = posts.map(post => (
+        <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+        </div>
+    ));
 
     return (
         <div>
             <h1>Posts</h1>
+            {postItems}
         </div>
     )
 }
