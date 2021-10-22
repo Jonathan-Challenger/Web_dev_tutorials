d3cb6f745fb84378732821215a3828026b96121a
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-22 12:41:19 +0100

    add form

--- redux-crash-course/src/components/Postform.js
+++ redux-crash-course/src/components/Postform.js
@@ -2,6 +2,19 @@ const Postform = () => {
     return (
         <div>
             <h1>Add Post</h1>
+            <form>
+                <div>
+                    <label>Title: </label><br />
+                    <input type="text" name="title" />
+                </div>
+                <br />
+                <div>
+                    <label>Body: </label><br />
+                    <textarea name="body" />
+                </div>
+                <br />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
