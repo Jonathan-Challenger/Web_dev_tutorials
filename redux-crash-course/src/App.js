0bc770bcdba5babb9ba255403ba0ebf80c1db5d6
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-22 15:04:38 +0100

    include provider in app and import store

--- redux-crash-course/src/App.js
+++ redux-crash-course/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Posts from './components/Posts';
 import Postform from './components/Postform';
+import { Provider } from 'react-redux';
+
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <Postform />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Postform />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
