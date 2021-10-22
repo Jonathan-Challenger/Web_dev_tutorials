3820839815c7b2c198026782ab33d878a4d5946b
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-22 12:39:01 +0100

    import postform into app

--- redux-crash-course/src/App.js
+++ redux-crash-course/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Posts from './components/Posts';
+import Postform from './components/Postform';
 
 function App() {
   return (
     <div className="App">
+      <Postform />
       <Posts />
     </div>
   );
