a703183592909cf598448a97420484499ab18d64
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-26 12:02:28 +0100

    add store to app and wrap in provider

--- MERN-stack-tutorial/client/src/App.js
+++ MERN-stack-tutorial/client/src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
   return (
+    <Provider store = {store}>
     <div className="App">
       <AppNavbar />
       <ShoppingList />
     </div>
+    </Provider>
   );
 }
 
