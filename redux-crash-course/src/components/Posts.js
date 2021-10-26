fc7b752c197734717c290ade53ce8c778a25278f
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-26 10:13:32 +0100

    add prop types

--- redux-crash-course/src/components/Posts.js
+++ redux-crash-course/src/components/Posts.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions/postActions";
 
@@ -20,6 +21,11 @@ const Posts = (props) => {
             {postItems}
         </div>
     )
+}
+
+Posts.propTypes = {
+    fetchPosts: PropTypes.func.isRequired,
+    posts: PropTypes.array.isRequired
 }
 
 const mapStateToProps = state => ({
