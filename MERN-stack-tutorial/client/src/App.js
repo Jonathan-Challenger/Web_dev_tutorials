03edc76344b678f1fb414c0ed65a482a914f8cb6
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-27 15:24:03 +0100

    remove space

--- MERN-stack-tutorial/client/src/App.js
+++ MERN-stack-tutorial/client/src/App.js
@@ -3,11 +3,11 @@ import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
 import ItemModal from './components/ItemModal';
 import { Container } from 'reactstrap';
-
 import { Provider } from 'react-redux';
 import store from './store';
 
 function App() {
+
   return (
     <Provider store = {store}>
     <div className="App">
