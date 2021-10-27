24f947b5ebef2fdf4588e230b72d49925b6d57e9
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-27 12:07:32 +0100

    add itemmodal to app and container to wrap itemmodal and shoppinglist

--- MERN-stack-tutorial/client/src/App.js
+++ MERN-stack-tutorial/client/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -10,7 +12,10 @@ function App() {
     <Provider store = {store}>
     <div className="App">
       <AppNavbar />
-      <ShoppingList />
+      <Container>
+        <ItemModal />
+        <ShoppingList />
+      </Container>
     </div>
     </Provider>
   );
