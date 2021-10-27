9451c641e99d8a74352a49f0e44604b872c3044b
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-27 10:36:23 +0100

    create skeleton of itemModal

---
+++ MERN-stack-tutorial/client/src/components/ItemModal.js
@@ -0,0 +1,11 @@
+import { Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input } from 'reactstrap';
+
+const ItemModal = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ItemModal
