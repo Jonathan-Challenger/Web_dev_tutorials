6bffb0d7aba02426ffc3704066ff4eef95d2fd76
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-27 12:07:16 +0100

    create component state and handletoggle handleonchange functions and add reactstrap form

--- MERN-stack-tutorial/client/src/components/ItemModal.js
+++ MERN-stack-tutorial/client/src/components/ItemModal.js
@@ -1,11 +1,43 @@
 import { Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input } from 'reactstrap';
+import { connect } from 'react-redux';
+import { addItem } from '../actions/itemActions';
+import { useState } from 'react';
 
 const ItemModal = () => {
+    const [modal, setModal] = useState(false);
+    const [name, setName] = useState('');
+
+    const handleToggle = () => {
+        setModal(!modal);
+    }
+
+    const handleOnChange = (e) => {
+        setName({ [e.target.name] : e.target.value })
+    }
+
     return (
         <div>
-            
+            <Button
+                color="dark"
+                style={{marginBottom: '2rem'}}
+                onClick={handleToggle}
+            >Add Item</Button>
+
+            <Modal isOpen={modal} toggle={handleToggle}>
+                <ModalHeader toggle={handleToggle}>Add To Shopping List</ModalHeader>
+                <ModalBody>
+                    <Form>
+                        <FormGroup>
+                            <Label for="item">Item</Label>
+                            <Input type="text" name="name" id="item" placeholder="Add shopping item" onChange={handleOnChange}>
+                            </Input>
+                            <Button color="dark" style={{marginTop:'2rem'}} block>Add Item</Button>
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+            </Modal>
         </div>
     )
 }
 
-export default ItemModal
+export default connect()(ItemModal)
